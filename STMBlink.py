877d79389ba650773e21ff5d437770ef8e09afba
Davit Alaverdyan <83835048+DavidAlaverdyan6@users.noreply.github.com>
2026-08-16 22:31:53 +0400

    Refactor STMBlink to use PC13 directly

--- STMBlink.py
+++ STMBlink.py
@@ -1,16 +1,10 @@
+from pins import PC13
 import time
-from telemetrix import telemetrix
 
-PINS = {'PC13': 31}
+board.set_pin_mode_digital_output(PC13)
 
-board = telemetrix.Telemetrix()
-board.set_pin_mode_digital_output(PINS['PC13'])
-
-# Flash PC13 LED 3 times
-for i in range(3):
-    board.digital_write(PINS['PC13'], 0) # Active LOW
+for _ in range(3):
+    board.digital_write(PC13, 0)
     time.sleep(0.3)
-    board.digital_write(PINS['PC13'], 1)
+    board.digital_write(PC13, 1)
     time.sleep(0.3)
-
-board.shutdown()
