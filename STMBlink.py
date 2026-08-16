3408548796bdb69bfcbdecbd40b01aa2ba83d20d
Davit Alaverdyan <83835048+DavidAlaverdyan6@users.noreply.github.com>
2026-08-16 22:48:29 +0400

    Update STMBlink.py

--- STMBlink.py
+++ STMBlink.py
@@ -1,6 +1,8 @@
-from pins import PC13
 import time
+from telemetrix import telemetrix
+from pins import PC13
 
+board = telemetrix.Telemetrix()
 board.set_pin_mode_digital_output(PC13)
 
 for _ in range(3):
@@ -8,3 +10,5 @@ for _ in range(3):
     time.sleep(0.3)
     board.digital_write(PC13, 1)
     time.sleep(0.3)
+
+board.shutdown()
